c2eaab5efc35d91d71d7724a960b10bffb300993
unknown <matheusfariasdasneves@gmail.com>
2023-10-03 13:39:16 -0300

    Enviando projeto finalizado enviando projeto finalizado, aplicado funcionalidades javascript.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Room-homepage
-Repositório para armazenar projeto/desafio de uma página inicial adquirido pelo site frontend mentor para treinar html, css e javascript.
+Repositório para armazenar projeto/desafio, adiquirido no site Frontend Mentor, de uma página inicial para treinar html, css e javascript.
+
+Desenvolvido em duas versões, mobile e dasktop, sendo a versão mobile somente para o tamanho de tela 375px e a versão dasktop 1300px, sendo assim, desconsiderado os demais tamanhos de telas, pois não é um projeto pensado em 100% responsivo, realizado somente para treinamento e testes.
--- js/main.js
+++ js/main.js
@@ -1,27 +1,115 @@
-const $topHeader = document.querySelector('.container-hearder');
-const $nav = document.querySelector('.nav');
-const $btnHamburguerMenu = document.querySelector('.hamburguer-icon');
-const $btnCloseMenu = document.querySelector('.close-icon');
-const $actionSlydeContainer = document.querySelector('.btns-actionSlyde-container');
+;(function(){
+    'use strict';
 
-init();
-$nav.style.display = 'flex';
-
-function init(){
-    $nav.style.display = 'none';
-    $nav.style.transform = 'translateY(-22em)';
-    $btnCloseMenu.removeAttribute('style');
-    $topHeader.style.display = 'flex';
+    const $topHeader = document.querySelector('.container-hearder');
+    const $nav = document.querySelector('.nav');
+    const $btnHamburguerMenu = document.querySelector('.hamburguer-icon');
+    const $btnCloseMenu = document.querySelector('.close-icon');
+    const $slydeShow = document.querySelector('.slyde-action');
+    const $actionSlydeContainer = document.querySelector('.btns-actionSlyde-container');
     $actionSlydeContainer.style.display = 'flex';
-};
 
-$btnHamburguerMenu.addEventListener('click', openMenu);
-$btnCloseMenu.addEventListener('click', closeMenu);
+    window.addEventListener('resize', checkViewSize);
+    window.addEventListener('DOMContentLoaded', checkViewSize);
 
-function openMenu(){
-    $nav.style.transform = 'translateY(-9.6em)';
-};
+    function checkViewSize(){
+        this.innerWidth >= 1300 ? viewDasktop() : viewMobile();
+        this.innerWidth < 1300 ? $nav.style.display = 'none' :  $nav.style.display = 'flex';
+    }
 
-function closeMenu(){
-    $nav.style.transform = 'translateY(-22em)';
-};
+    function onCarousel(){
+        document.querySelector('.off-js').classList.remove('off-js');
+    }
+
+    let click = 0;
+    function slydeActionDasktop(e){
+        let target = e.target.getAttribute('action'); 
+
+        const actions = {
+            nextBtn: function(){
+                
+                if(click === 0){
+                    $slydeShow.style.transform = 'translateX(-125em)';
+                }else if(click === 1){
+                    $slydeShow.style.transform = 'translateX(-250em)';
+                }else{
+                    return
+                };
+                click++;
+            },
+            previousBtn: function(){
+                if(click === 0){
+                    return;
+                }
+                else if($slydeShow.style.transform === 'translateX(-125em)'){
+                    $slydeShow.style.transform = 'translateX(0em)';
+                }else if($slydeShow.style.transform = 'translateX(-250em)'){
+                    $slydeShow.style.transform = 'translateX(-125em)';
+                }
+                click--;
+            }
+        };
+
+        if(target){
+            actions[target]();
+        };
+    };
+
+    function slydeActionMobile(e){
+        let target = e.target.getAttribute('action'); 
+
+        const actions = {
+            nextBtn: function(){
+                
+                if(click === 0){
+                    $slydeShow.style.transform = 'translateX(-34em)';
+                }else if(click === 1){
+                    $slydeShow.style.transform = 'translateX(-68em)';
+                }else{
+                    return
+                };
+                click++;
+            },
+            previousBtn: function(){
+                if(click === 0){
+                    return;
+                }
+                else if($slydeShow.style.transform === 'translateX(-34em)'){
+                    $slydeShow.style.transform = 'translateX(0em)';
+                }else if($slydeShow.style.transform = 'translateX(-68em)'){
+                    $slydeShow.style.transform = 'translateX(-34em)';
+                }
+                click--;
+            }
+        };
+
+        if(target){
+            actions[target]();
+        };
+    };
+
+    function viewMobile(){
+        $nav.style.transform = 'translateY(-22em)';
+        $topHeader.style.display = 'flex';
+        $btnHamburguerMenu.addEventListener('click', openMenu);
+        $btnCloseMenu.addEventListener('click', closeMenu);
+        $actionSlydeContainer.addEventListener('click', slydeActionMobile);
+
+        function openMenu(){
+            $nav.style.display = 'flex';
+            $nav.style.transform = 'translateY(-9.6em)';
+            
+        };
+
+        function closeMenu(){
+            $nav.style.transform = 'translateY(-22em)';
+        };
+    };
+
+    function viewDasktop(){
+        $nav.style.display = 'flex';
+        $actionSlydeContainer.addEventListener('click', slydeActionDasktop);
+    };
+
+    onCarousel();
+})();
